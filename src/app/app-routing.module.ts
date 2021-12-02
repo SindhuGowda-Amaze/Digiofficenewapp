b8bccb41cc65da89a726fab28309a9f9c8b65080
Sindhu Gowda <SindhuGowda.amazeInc@gmail.com>
2021-12-02 10:50:26 +0530

    Sindhu Changes, added companyprofile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { SidebarComponent } from 'src/pages/sidebar/sidebar.component';
+import { CompanyprofileComponent } from './companyprofile/companyprofile.component';
+import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
 
   { path: "", redirectTo: "Login", pathMatch: "full" },
   {
     path: "Sidebar", component: SidebarComponent
+  },
+  {
+    path: "Header", component: HeaderComponent
+  },
+  {
+    path: "Companyprofile", component: CompanyprofileComponent
   },
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from '../pages/sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
+import { CompanyprofileComponent } from './companyprofile/companyprofile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    HeaderComponent
+    HeaderComponent,
+    CompanyprofileComponent
   ],
   imports: [
     BrowserModule,
