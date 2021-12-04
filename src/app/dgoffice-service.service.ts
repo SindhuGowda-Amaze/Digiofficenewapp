a9c9ce60027b25b9a65342766d736f332007517c
Rupali Pawar <naveen.amazeinc@gmail.com>
2021-12-04 13:23:42 +0530

    Naveen

--- src/app/dgoffice-service.service.ts
+++ src/app/dgoffice-service.service.ts
@@ -47,6 +47,24 @@ export class DgofficeServiceService {
     return this.http.get<any[]>(APIURL);
   }
 
+  public InsertBanks(json:any) {
+    debugger
+    let APIURL = this.baseURL+"Master/InsertBanks";
+    return this.http.post<any[]>(APIURL, json);
+  }
+
+  public GetPayGroup() {
+    debugger
+    let APIURL = this.baseURL+"Master/GetPayGroup";
+    return this.http.get<any[]>(APIURL);
+  }
+
+  public InsertPayGroup(json:any) {
+    debugger
+    let APIURL = this.baseURL+"Master/InsertPayGroup";
+    return this.http.post<any[]>(APIURL, json);
+  }
+
 
 
 }
